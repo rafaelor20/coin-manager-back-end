1827ce9930c94574212e4c5a4fbe135128312896
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-06-16 17:56:38 -0300

    feat/ test route /transactions/store

--- tests/integration/transaction.test.ts
+++ tests/integration/transaction.test.ts
@@ -12,6 +12,11 @@ beforeAll(async () => {
   await cleanDb();
 });
 
+beforeEach(async () => {
+  await init();
+  await cleanDb();
+});
+
 const server = supertest(app);
 
 describe('GET /transactions/historic', () => {
@@ -49,14 +54,61 @@ describe('GET /transactions/historic', () => {
       expect(response.status).toBe(httpStatus.OK);
       expect(response.body).toEqual([
         {
-          id: transaction.id,
+          id: expect.any(Number),
           user_id: transaction.user_id,
           description: transaction.description,
           amount: transaction.amount,
-          date: transaction.date.toISOString(),
+          date: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/),
+          //date: transaction.date.toISOString(),
           category: transaction.category,
         },
       ]);
+    });
+  });
+});
+
+describe('POST /transactions/store', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/transactions/store');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/transactions/store').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/transactions/store').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 201 and create store', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const transaction = await createTransaction(user);
+      const body = transaction;
+      const response = await server.post('/transactions/store').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toBe(httpStatus.CREATED);
+      expect(response.body).toEqual({
+        id: expect.any(Number),
+        user_id: transaction.user_id,
+        description: transaction.description,
+        amount: transaction.amount,
+        date: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/),
+        //date: transaction.date.toISOString(),
+        category: transaction.category,
+      });
     });
   });
 });
