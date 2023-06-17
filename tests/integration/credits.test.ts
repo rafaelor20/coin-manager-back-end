6dc5eddd2f8c97a4fcfdbb042ac38c512c93d89c
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-06-16 22:43:05 -0300

    feat/ tests routes store debts and store credits

--- tests/integration/credits.test.ts
+++ tests/integration/credits.test.ts
@@ -65,3 +65,48 @@ describe('GET /credits', () => {
     });
   });
 });
+
+describe('POST /credits/store', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/credits/store');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/credits/store').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/credits/store').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 201 and create store', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const credit = await createCredit(user);
+      const body = credit;
+      const response = await server.post('/credits/store').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toBe(httpStatus.CREATED);
+      expect(response.body).toEqual({
+        id: expect.any(Number),
+        user_id: credit.user_id,
+        debtor: credit.debtor,
+        amount: credit.amount,
+        createdAt: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/),
+        payDate: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/),
+      });
+    });
+  });
+});
--- tests/integration/debts.test.ts
+++ tests/integration/debts.test.ts
@@ -65,3 +65,48 @@ describe('GET /debts', () => {
     });
   });
 });
+
+describe('POST /debts/store', () => {
+  it('should respond with status 401 if no token is given', async () => {
+    const response = await server.post('/debts/store');
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if given token is not valid', async () => {
+    const token = faker.lorem.word();
+
+    const response = await server.post('/debts/store').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  it('should respond with status 401 if there is no session for given token', async () => {
+    const userWithoutSession = await createUser();
+    const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+    const response = await server.post('/debts/store').set('Authorization', `Bearer ${token}`);
+
+    expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+  });
+
+  describe('when token is valid', () => {
+    it('should respond with status 201 and create store', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const debt = await createDebt(user);
+      const body = debt;
+      const response = await server.post('/debts/store').set('Authorization', `Bearer ${token}`).send(body);
+
+      expect(response.status).toBe(httpStatus.CREATED);
+      expect(response.body).toEqual({
+        id: expect.any(Number),
+        user_id: debt.user_id,
+        creditor: debt.creditor,
+        amount: debt.amount,
+        createdAt: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/),
+        payDate: expect.stringMatching(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{3})?Z$/),
+      });
+    });
+  });
+});
