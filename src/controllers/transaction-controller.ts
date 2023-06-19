fd27f97e271fabe860158456646e8c7435193891
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2023-06-18 21:13:46 -0300

    feat/ docker config

--- src/controllers/transaction-controller.ts
+++ src/controllers/transaction-controller.ts
@@ -4,8 +4,9 @@ import { AuthenticatedRequest } from '@/middlewares';
 import transactionService from '@/services/transaction-service';
 
 export async function getHistoric(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { userId } = req;
   try {
-    const { userId } = req;
+    console.log(userId);
     const historic = await transactionService.getHistoric(userId);
     return res.status(httpStatus.OK).send(historic);
   } catch (error) {
@@ -14,9 +15,11 @@ export async function getHistoric(req: AuthenticatedRequest, res: Response, next
 }
 
 export async function storeTransaction(req: AuthenticatedRequest, res: Response, next: NextFunction) {
-  try {
-    const { userId } = req;
-    const { user_id, description, amount, category } = req.body;
+  const { userId } = req;
+  try {  
+    const user_id = userId;
+    const { description, amount, category } = req.body;
+    console.log({user_id, description, amount, category})
     const transaction = await transactionService.storeTransaction(userId, { user_id, description, amount, category });
     return res.status(httpStatus.CREATED).send(transaction);
   } catch (error) {
